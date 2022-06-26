25cb1ba2fe667b12528e4951a3b0109c4f758c9d
Cameobc <dkqtk@naver.com>
2022-06-26 22:21:51 +0900

    #임시커밋

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.xml.transform.Result;
 import java.util.List;
 
 public class JpaMain {
@@ -22,42 +23,14 @@ public class JpaMain {
         tx.begin();
         
         try {
-          /*  Member member = new Member();
-            member.setId(2l);
-            member.setName("helloB");
-            em.persist(member);*/
-           /* Member findMember = em.find(Member.class, 1L);
-            // 변경 사항이 있으면 update 쿼리를 날린다.
-            findMember.setName("HelloJPA");
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());*/
-            // 대상이 테이블이 아닌 객체가 된다.
-            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
-*/
+            //영속
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            // 비영속상태
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+            //영속 엔티티의 동일성 보장
+            System.out.println("Result = " + (findMember1 == findMember2));
 
-            //영속상태 -> 실제 db 에 저장된 상태는 아니다.
-            // 영속성 컨텍스트는 내부에 1차 캐시를 들고 있음
-            System.out.println("===BEFORE===");
-            em.persist(member);
-            System.out.println("===After===");
-
-            // select 쿼리가 안나갔음 -> 1차 캐시에서 가지고 있어서.
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
 
             // 트랜잭션 커밋 시점에 db에 쿼리가 날아간다.
             tx.commit();
