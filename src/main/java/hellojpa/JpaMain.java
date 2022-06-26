9246898c9213cc73135fdef3840cc07e2139643c
Cameobc <dkqtk@naver.com>
2022-06-26 22:12:59 +0900

    #임시커밋

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -32,7 +32,7 @@ public class JpaMain {
             System.out.println("findMember.getId() = " + findMember.getId());
             System.out.println("findMember.getName() = " + findMember.getName());*/
             // 대상이 테이블이 아닌 객체가 된다.
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(1)
                     .setMaxResults(10)
                     .getResultList();
@@ -40,7 +40,26 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.println("member.getName() = " + member.getName());
             }
-            
+*/
+
+            // 비영속상태
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            //영속상태 -> 실제 db 에 저장된 상태는 아니다.
+            // 영속성 컨텍스트는 내부에 1차 캐시를 들고 있음
+            System.out.println("===BEFORE===");
+            em.persist(member);
+            System.out.println("===After===");
+
+            // select 쿼리가 안나갔음 -> 1차 캐시에서 가지고 있어서.
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // 트랜잭션 커밋 시점에 db에 쿼리가 날아간다.
             tx.commit();
         }catch (Exception e){
             e.printStackTrace();
